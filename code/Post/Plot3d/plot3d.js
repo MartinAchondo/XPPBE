3b3e218591f9bb0ad16cd3ef915bf1ef65944e5b
martinachondo <acho.martin@gmail.com>
2023-10-09 22:58:12 -0300

    m

--- code/Post/Plot3d/plot3d.js
+++ code/Post/Plot3d/plot3d.js
@@ -1,12 +1,9 @@
-// Function to handle file selection and plotting
 function handleFileSelect(event) {
     const files = event.target.files;
 
     if (files.length > 0) {
-        // Create an array to store data and traces for each file
         const dataTraces = [];
 
-        // Function to generate a random color
         function getRandomColor() {
             return `#${Math.floor(Math.random()*16777215).toString(16)}`;
         }
@@ -20,10 +17,9 @@ function handleFileSelect(event) {
             'test': 'red'
         };
         
-        // Function to create a trace for a file with a specific color
         function createTrace(data, label) {
             const color = color_dict[label] || getRandomColor();
-        // Function to create a trace for a file
+
             const trace = {
                 x: data.map(row => row.X),
                 y: data.map(row => row.Y),
@@ -35,28 +31,25 @@ function handleFileSelect(event) {
                     opacity: 0.7,
                     color: color
                 },
-                name: label // Use the file name as the trace label
+                name: label 
             };
             return trace;
         }
 
-        // Loop through each selected file
         for (let i = 0; i < files.length; i++) {
             const file = files[i];
             const reader = new FileReader();
 
             reader.onload = function (e) {
-                // Parse the CSV data when the file is loaded
                 Papa.parse(e.target.result, {
                     header: true,
                     dynamicTyping: true,
                     complete: function (results) {
                         const data = results.data;
-                        const fileName = file.name.replace(/\.[^/.]+$/, ''); // Remove file extension
+                        const fileName = file.name.replace(/\.[^/.]+$/, '');
                         const trace = createTrace(data, fileName);
                         dataTraces.push(trace);
 
-                        // If all files have been processed, create the plot
                         if (dataTraces.length === files.length) {
                             const layout = {
                                 scene: {
@@ -77,11 +70,9 @@ function handleFileSelect(event) {
                 });
             };
 
-            // Read the selected file as text
             reader.readAsText(file);
         }
     }
 }
 
-// Attach the event listener to the file input element
 document.getElementById('file-input').addEventListener('change', handleFileSelect, false);
