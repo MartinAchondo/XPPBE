01a5413bf204352208779c3cac471b4d38d3a15d
martinachondo <acho.martin@gmail.com>
2023-12-05 15:02:01 -0300

    born ion

--- code/Model/Mesh/born_ion/create_sphere.py
+++ code/Model/Mesh/born_ion/create_sphere.py
@@ -13,11 +13,11 @@ vertex_normals = vertices / np.linalg.norm(vertices, axis=1)[:, np.newaxis]
 
 file_path = os.path.dirname(os.path.realpath(__file__))
 
-with open(os.path.join(file_path,'sphere.vert'), 'w') as vertex_file:
+with open(os.path.join(file_path,'born_ion.vert'), 'w') as vertex_file:
     for vertex,normal in zip(vertices,vertex_normals):
         vertex_file.write(f"{vertex[0]:.6f} {vertex[1]:.6f} {vertex[2]:.6f} {normal[0]:.6f} {normal[1]:.6f} {normal[2]:.6f} {0} {1} {0}\n")
 
-with open(os.path.join(file_path,'sphere.face'), 'w') as faces_file:
+with open(os.path.join(file_path,'born_ion.face'), 'w') as faces_file:
     for face in faces:
         faces_file.write(f"{face[0]} {face[1]} {face[2]} 1 1 \n")
 
