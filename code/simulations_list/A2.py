59cfff553693d708e21589e014de0d987d2035b5
martinachondo <acho.martin@gmail.com>
2024-01-09 11:55:17 -0300

    sims

--- code/simulations_list/A2.py
+++ code/simulations_list/A2.py
@@ -47,11 +47,11 @@ class PDE():
                                 'T' : 300 
                                 }
                 
-                self.N_points = {'dx_interior': 0.4,
-                                'dx_exterior': 2,
+                self.N_points = {'dx_interior': 0.3,
+                                'dx_exterior': 1.4,
                                 'N_border': 15,
                                 'dR_exterior': 10,
-                                'dx_experimental': 2,
+                                'dx_experimental': 1.4,
                                 'N_pq': 70,
                                 'G_sigma': 0.04,
                                 'mesh_density': 6
