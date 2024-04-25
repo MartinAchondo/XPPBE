8bfc7a4264d8102ccb3f779e98e26166d8dcda8b
martinachondo <acho.martin@gmail.com>
2024-04-25 15:39:12 -0400

    fixes

---
+++ xppbe/__init__.py
@@ -0,0 +1,8 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+os.environ['TF_RUN_EAGER_OP_AS_FUNCTION']='false'
+import tensorflow as tf
+tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
+
+xppbe_path = os.path.dirname(os.path.abspath(__file__))
+
