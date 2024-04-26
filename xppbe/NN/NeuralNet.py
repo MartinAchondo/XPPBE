b3369e75795665ead9a39548c0b4b8f918eb06f2
martinachondo <acho.martin@gmail.com>
2024-04-26 15:31:00 -0400

    RWF implemented

--- xppbe/NN/NeuralNet.py
+++ xppbe/NN/NeuralNet.py
@@ -50,7 +50,6 @@ class NeuralNet(tf.keras.Model):
                  output_dim=1,
                  num_hidden_layers=2,
                  num_neurons_per_layer=20,
-                 trainable=True,
                  num_hidden_blocks=2,
                  activation='tanh',
                  adaptative_activation=False,
@@ -59,31 +58,41 @@ class NeuralNet(tf.keras.Model):
                  fourier_features=False, 
                  num_fourier_features=128, 
                  fourier_sigma=1,
-                 scale=([-1.,-1.,-1.],[1.,1.,1.]),
+                 weight_factorization=False,
                  scale_input=True,
                  scale_NN=1.0,
+                 scale=([-1.,-1.,-1.],[1.,1.,1.]),
                  **kwargs):
         super().__init__(**kwargs)
 
         self.input_shape_N = input_shape
-        self.num_hidden_layers = num_hidden_layers
-        self.num_hidden_blocks = num_hidden_blocks
         self.output_dim = output_dim
+        self.num_hidden_layers = num_hidden_layers
+        self.num_neurons_per_layer = num_neurons_per_layer
+        self.num_hidden_blocks = num_hidden_blocks
+        
+        self.activation = activation
+        self.adaptative_activation = adaptative_activation
+
+        self.kernel_initializer = kernel_initializer
+        self.architecture_Net = architecture_Net
+        self.use_fourier_features = fourier_features
+        self.num_fourier_features = num_fourier_features
+        self.fourier_sigma = fourier_sigma
+        self.weight_factorization = weight_factorization
+
+        self.scale_input = scale_input
+        self.scale_NN = scale_NN
         self.lb = tf.constant(scale[0])
         self.ub = tf.constant(scale[1])
-        self.scale_NN = scale_NN
-        self.architecture_Net = architecture_Net
-        self.scale_input = scale_input
-        self.num_fourier_features = num_fourier_features
-        self.sigma = fourier_sigma
-        self.use_fourier_features = fourier_features
 
-        if kernel_initializer == 'uniform':
-            kernel_initializer = tf.keras.initializers.RandomUniform(minval=-1, maxval=1)
-        
+        if not self.weight_factorization:
+            Dense_Layer = tf.keras.layers.Dense
+        elif self.weight_factorization:
+            Dense_Layer = CustomDenseLayer
 
         # Scale layer
-        if scale_input:
+        if self.scale_input:
             self.scale = tf.keras.layers.Lambda(
                 lambda x: 2.0 * (x - self.lb) / (self.ub - self.lb) - 1.0, 
                 name=f'scale_layer')
@@ -95,80 +104,75 @@ class NeuralNet(tf.keras.Model):
                                                           activation=None, 
                                                           use_bias=False,
                                                           trainable=False, 
-                                                          kernel_initializer=tf.initializers.RandomNormal(stddev=self.sigma),
+                                                          kernel_initializer=tf.initializers.RandomNormal(stddev=self.fourier_sigma),
                                                           name='fourier_features'))
             class SinCosLayer(tf.keras.layers.Layer):
                 def call(self, Z):
                     return tf.concat([tf.sin(2.0*np.pi*Z), tf.cos(2.0*np.pi*Z)], axis=-1)
             self.fourier_features.add(SinCosLayer(name='fourier_sincos_layer'))
-
-        # Custom activation function
-        class CustomActivation(tf.keras.layers.Layer):
-            def __init__(self, units=1, adaptative_activation=False, **kwargs):
-                super(CustomActivation, self).__init__(**kwargs)
-                self.units = units
-                self.adaptative_activation = adaptative_activation
-            def build(self, input_shape):
-                self.a = self.add_weight(name='a',
-                                        shape=(self.units,),
-                                        initializer='ones',
-                                        trainable=self.adaptative_activation)
-                super(CustomActivation, self).build(input_shape)
-            def call(self, inputs):
-                a_expanded = tf.expand_dims(self.a, axis=0) 
-                activation_func = tf.keras.activations.get(activation)
-                return activation_func(inputs * a_expanded)
-
-        # FCNN architecture
+        
+        # FCNN or ModMLP architectures
         if self.architecture_Net in ('FCNN','ModMLP'):
             self.hidden_layers = list()
             for i in range(self.num_hidden_layers):
-                layer = tf.keras.layers.Dense(num_neurons_per_layer,
-                                              activation=CustomActivation(units=num_neurons_per_layer,adaptative_activation=adaptative_activation),
-                                              kernel_initializer=kernel_initializer,
-                                              trainable=trainable,
-                                              name=f'layer_{i}')
+                layer = Dense_Layer(self.num_neurons_per_layer,
+                            activation=CustomActivation(units=self.num_neurons_per_layer,
+                                                        activation=self.activation, 
+                                                        adaptative_activation=self.adaptative_activation),
+                            kernel_initializer=self.kernel_initializer,
+                            name=f'layer_{i}')
                 self.hidden_layers.append(layer)
+
+            # ModMLP architecture
+            if self.architecture_Net == 'ModMLP':
+                self.U = Dense_Layer(self.num_neurons_per_layer,
+                            activation=CustomActivation(units=self.num_neurons_per_layer,
+                                                        activation=self.activation, 
+                                                        adaptative_activation=self.adaptative_activation),
+                            kernel_initializer=self.kernel_initializer,
+                            name=f'layer_u')
+                self.V = Dense_Layer(self.num_neurons_per_layer,
+                            activation=CustomActivation(units=self.num_neurons_per_layer,
+                                                        activation=self.activation, 
+                                                        adaptative_activation=self.adaptative_activation),
+                            kernel_initializer=self.kernel_initializer,
+                            name=f'layer_v')
 
         # ResNet architecture
         elif self.architecture_Net == 'ResNet':
-            self.first = tf.keras.layers.Dense(num_neurons_per_layer,
-                                               activation=CustomActivation(units=num_neurons_per_layer,adaptative_activation=adaptative_activation),
-                                               kernel_initializer=kernel_initializer,
-                                               name=f'layer_0')
+            self.first = Dense_Layer(self.num_neurons_per_layer,
+                            activation=CustomActivation(units=self.num_neurons_per_layer,
+                                                        activation=self.activation, 
+                                                        adaptative_activation=self.adaptative_activation),
+                            kernel_initializer=self.kernel_initializer,
+                            name=f'layer_0')
             self.hidden_blocks = list()
             self.hidden_blocks_activations = list()
             for i in range(self.num_hidden_blocks):
                 block = tf.keras.Sequential(name=f"block_{i}")
-                block.add(tf.keras.layers.Dense(num_neurons_per_layer,
-                                                activation=CustomActivation(units=num_neurons_per_layer,adaptative_activation=adaptative_activation),
-                                                kernel_initializer=kernel_initializer))
-                block.add(tf.keras.layers.Dense(num_neurons_per_layer,
-                                                activation=None,
-                                                kernel_initializer=kernel_initializer))
+                block.add(Dense_Layer(self.num_neurons_per_layer,
+                                activation=CustomActivation(units=self.num_neurons_per_layer,
+                                                            activation=self.activation,
+                                                            adaptative_activation=self.adaptative_activation),
+                                kernel_initializer=self.kernel_initializer))
+                block.add(Dense_Layer(self.num_neurons_per_layer,
+                                activation=None,
+                                kernel_initializer=self.kernel_initializer))
                 self.hidden_blocks.append(block)
-                activation_layer = tf.keras.layers.Activation(activation=CustomActivation(units=num_neurons_per_layer,adaptative_activation=adaptative_activation))
+                activation_layer = tf.keras.layers.Activation(activation=CustomActivation(units=self.num_neurons_per_layer,
+                                                                                          activation=self.activation,
+                                                                                          adaptative_activation=self.adaptative_activation))
                 self.hidden_blocks_activations.append(activation_layer)
             
-            self.last = tf.keras.layers.Dense(num_neurons_per_layer,
-                                              activation=CustomActivation(units=num_neurons_per_layer,adaptative_activation=adaptative_activation),
-                                              kernel_initializer=kernel_initializer,
-                                              name=f'layer_1')
-
-        if architecture_Net == 'ModMLP':
-            self.U = tf.keras.layers.Dense(num_neurons_per_layer,
-                                              activation=CustomActivation(units=num_neurons_per_layer,adaptative_activation=adaptative_activation),
-                                              kernel_initializer=kernel_initializer,
-                                              trainable=trainable,
-                                              name=f'layer_u')
-            self.V = tf.keras.layers.Dense(num_neurons_per_layer,
-                                              activation=CustomActivation(units=num_neurons_per_layer,adaptative_activation=adaptative_activation),
-                                              kernel_initializer=kernel_initializer,
-                                              trainable=trainable,
-                                              name=f'layer_v')
+            self.last = Dense_Layer(self.num_neurons_per_layer,
+                            activation=CustomActivation(units=self.num_neurons_per_layer,
+                                                        activation=self.activation, 
+                                                        adaptative_activation=self.adaptative_activation),
+                            kernel_initializer=self.kernel_initializer,
+                            name=f'layer_1')
 
         # Output layer
-        self.out = tf.keras.layers.Dense(output_dim, name=f'output_layer')
+        self.out = Dense_Layer(output_dim, name=f'output_layer')
 
         # Scale output layer
         self.scale_out = tf.keras.layers.Lambda(
@@ -222,3 +226,61 @@ class NeuralNet(tf.keras.Model):
         X = self.out(X)
         return self.scale_out(X)
     
+
+class CustomActivation(tf.keras.layers.Layer):
+
+    def __init__(self, units=1, activation='tanh', adaptative_activation=False, **kwargs):
+        super(CustomActivation, self).__init__(**kwargs)
+        self.units = units
+        self.activation = activation
+        self.adaptative_activation = adaptative_activation
+
+    def build(self, input_shape):
+        self.a = self.add_weight(name='a',
+                                shape=(self.units,),
+                                initializer='ones',
+                                trainable=self.adaptative_activation)
+        super(CustomActivation, self).build(input_shape)
+
+    def call(self, inputs):
+        a_expanded = tf.expand_dims(self.a, axis=0) 
+        activation_func = tf.keras.activations.get(self.activation)
+        return activation_func(inputs * a_expanded)
+
+class CustomDenseLayer(tf.keras.layers.Layer):
+
+    def __init__(self, units, activation=None, kernel_initializer='glorot_normal', **kwargs):
+        super(CustomDenseLayer, self).__init__(**kwargs)
+        self.units = units
+        self.activation = activation
+        self.kernel_initializer= tf.keras.initializers.get(kernel_initializer)
+                                        
+    def build(self, input_shape):
+        self.input_dim = input_shape[-1]
+        W = tf.Variable(initial_value=self.kernel_initializer(shape=(input_shape[-1], self.units)),
+                trainable=False)
+        S, V = self.weight_factorization(W)
+        self.S = self.add_weight(name='S', shape=S.shape, initializer=tf.constant_initializer(S.numpy()), trainable=True)
+        self.V = self.add_weight(name='V', shape=V.shape, initializer=tf.constant_initializer(V.numpy()), trainable=True)
+        self.b = self.add_weight(name='b', shape=(self.units,), initializer='zeros', trainable=True)
+
+    def weight_factorization(self, W, mean=1.0, stddev=0.1):
+        S = mean + tf.random.normal(shape=[tf.shape(W)[-1]], stddev=stddev)
+        S = tf.exp(S)
+        V = W / S
+        return S,V
+
+    def call(self, inputs):
+        SV = tf.multiply(self.S, self.V)
+        outputs = tf.matmul(inputs, SV) + self.b
+        if self.activation is not None:
+            outputs = self.activation(outputs)
+        return outputs
+
+
+
+if __name__=='__main__':
+    nn = NeuralNet(weight_factorization=True,adaptative_activation=True)
+    nn.build_Net()
+    nn(tf.constant([[1.0,2.0,-5]], dtype='float32'))
+    nn.save_weights('test')
