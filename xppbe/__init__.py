da2f32199231eaee9a6d3202927afbc1eb101e04
martinachondo <acho.martin@gmail.com>
2024-05-23 12:15:14 -0400

    check tests

--- xppbe/__init__.py
+++ xppbe/__init__.py
@@ -4,5 +4,7 @@ os.environ['TF_RUN_EAGER_OP_AS_FUNCTION']='false'
 # import tensorflow as tf
 # tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
 
+from xppbe.Simulation import Simulation
+
 xppbe_path = os.path.dirname(os.path.abspath(__file__))
 
